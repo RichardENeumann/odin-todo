0b477ef4cf90305d57d75bd3e8610805d7bfc80b
RichardENeumann <RichardNeumann@googlemail.com>
2024-03-04 10:09:47 +0100

    Implement saving snapshot to localStorage

--- src/logic/inc-storagehandler.js
+++ src/logic/inc-storagehandler.js
@@ -13,6 +13,7 @@ function importSnapshot() {
 function exportSnapshot() {
     let processedSnapshot = JSON.stringify(snapshot, null, 2);
     
+    // Download JSON file 
     let element = document.createElement("a");
         element.setAttribute("href", "data:application/json;charset=utf-8," + encodeURIComponent(processedSnapshot));
         element.setAttribute("download", "tadaSnapshot-" + new Date().toISOString().split('T')[0]);
@@ -20,11 +21,13 @@ function exportSnapshot() {
         document.body.appendChild(element);
         element.click();
         document.body.removeChild(element);
+
     document.getElementById("dlgExported").showModal();
 }
 
 function saveState() {
-    console.log("Save");
+    localStorage.clear();
+    localStorage.setItem("localSnapshot", JSON.stringify(snapshot, null, 2));
 }
 
 loadSnapshot();
