ddbfc070e0b413f4cdbe5f399bcd5cba29b6bb68
RichardENeumann <RichardNeumann@googlemail.com>
2024-03-06 12:42:21 +0100

    Implement AddTask button pt2

--- src/logic/inc-task.js
+++ src/logic/inc-task.js
@@ -2,11 +2,24 @@ import { snapshot } from "../index.js";
 
 export { createTask };
 
+function findUnusedId() {
+    // find unused id in snapshot.tasks
+    return 10;
+}
+
 function createTask(title = "Example Task") {
-    let todo = new Date();
-    console.log(snapshot.tasks);
     let newTask = {
         title, 
-        todo     
+        "id": findUnusedId(),
+        "todo" : new Date().toISOString(),  
+        "doing": false,   
+        "done": false
     };
+    // Check if snapshot is empty before pushing new task
+    if ("tasks" in snapshot) {
+        snapshot.tasks.push(newTask)    
+    } else {
+        snapshot.tasks = [];
+        snapshot.tasks.push(newTask)
+    }
 }
