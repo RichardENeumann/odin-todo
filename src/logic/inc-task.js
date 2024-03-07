4a6fece0b2653895d297b9da29dee50be3b1d97c
RichardENeumann <RichardNeumann@googlemail.com>
2024-03-07 11:14:24 +0100

    Implement findUnusedId function

--- src/logic/inc-task.js
+++ src/logic/inc-task.js
@@ -3,8 +3,15 @@ import { snapshot } from "../index.js";
 export { createTask };
 
 function findUnusedId() {
-    // find unused id in snapshot.tasks
-    return 10;
+    // First sort all id numbers into an array by ascending order
+    let helper = snapshot.tasks.toSorted((a, b) => (a.id < b.id) ? -1 : (a.id > b.id) ? 1 : 0);
+
+    // Find first unused id by comparing index to id key
+    let unusedId = 0;
+    while (unusedId < helper.length && unusedId === helper[unusedId].id) {
+        unusedId++;
+    }
+    return unusedId;
 }
 
 function createTask(title = "Example Task") {
