5a9349bd52cf2789106e92c5e3f4b459f119ccad
RichardENeumann <RichardNeumann@googlemail.com>
2024-03-26 09:35:55 +0100

    Implemenet fileReader as promise to move render logic back to index.js

--- src/logic/index.js
+++ src/logic/index.js
@@ -20,7 +20,7 @@ import {
 
 // Initialize
 const snapshot = loadOnStartup();
-renderVersionNumber("v0.3.0");
+renderVersionNumber("v0.3.2");
 renderToAppConsole("Status nominal");
 renderToDisplay();
 
@@ -28,11 +28,8 @@ renderToDisplay();
 // Lefthand group
 // Save current state of snapshot object to localStorage
 document.getElementById("btSave").addEventListener("click", () => {
-  if (saveState(snapshot)) {
-    renderToAppConsole("Saved to localStorage");
-  } else {
-    renderToAppConsole("Nothing to save");
-  }
+  saveState(snapshot);
+  renderToAppConsole("Saved to localStorage");
 });
 
 // Open file selector dialog
@@ -40,10 +37,15 @@ const dlgImport = document.getElementById("dlgImport");
 document.getElementById("btImport").addEventListener("click", () => dlgImport.showModal());
 
 // If file is selected, load JSON, update local snapshot object, then re-render
-document.getElementById("btConfirmImport").addEventListener("click", () => {
+document.getElementById("btConfirmImport").addEventListener("click", async () => {
   const fileSelector = document.getElementById("fileSelector");
   if (fileSelector.files.length !== 0) {
-    importSnapshot(snapshot, fileSelector);
+    const result = await importSnapshot(fileSelector);
+    const parsedResult = JSON.parse(result);
+    snapshot.projects = parsedResult.projects;
+    snapshot.tasks = parsedResult.tasks;
+    snapshot.options = parsedResult.options;
+    renderToDisplay();
     renderToAppConsole("Snapshot imported");
   } else {
     renderToAppConsole("Nothing to import");
@@ -53,12 +55,9 @@ document.getElementById("btConfirmImport").addEventListener("click", () => {
 
 // Export snapshot to JSON file and show confirmation dialog on success
 document.getElementById("btExport").addEventListener("click", () => {
-  if (exportSnapshot(snapshot)) {
-    renderToAppConsole("Exported to JSON file");
-    document.getElementById("dlgExportSuccess").showModal();
-  } else {
-    renderToAppConsole("Nothing exported");
-  }
+  exportSnapshot(snapshot);
+  renderToAppConsole("Exported to JSON file");
+  document.getElementById("dlgExportSuccess").showModal();
 });
 
 // Middle group
